cfb529dff8e4cf369a5e3e92541789d38d7777d7
Sascha Lisson <sl@slisson.de>
2019-08-26 15:41:19 +0200

    Fix update-gcloud.sh

--- update-gcloud.sh
+++ update-gcloud.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 kubectx gcloud
-export DOCKER_TLS_VERIFY="0"
-export DOCKER_HOST="unix:///var/run/docker.sock"
+unset DOCKER_TLS_VERIFY
+unset DOCKER_HOST
+unset DOCKER_CERT_PATH
+
 ./docker-build-all.sh
+
 docker tag webmps-ui eu.gcr.io/cloudmps-250805/webmps-ui
 docker tag webmps-model eu.gcr.io/cloudmps-250805/webmps-model
 docker tag webmps-db eu.gcr.io/cloudmps-250805/webmps-db
+
+docker push eu.gcr.io/cloudmps-250805/webmps-db
+docker push eu.gcr.io/cloudmps-250805/webmps-model
+docker push eu.gcr.io/cloudmps-250805/webmps-ui
+
 ./kubernetes-apply-gcloud.sh
