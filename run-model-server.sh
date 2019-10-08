08cb7ff18d78da0f3f2d09c0b7a70cf822e95eb5
Sascha Lisson <sl@slisson.de>
2019-10-08 13:07:01 +0200

    -XX:+UseCGroupMemoryLimitForHeap is not required with OpenJDK 11

--- run-model-server.sh
+++ run-model-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Djdbc.url=$jdbc_url -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -cp "model-server/build/libs/*" de.q60.webmps.server.Main
+java -Djdbc.url=$jdbc_url -cp "model-server/build/libs/*" de.q60.webmps.server.Main
--- run-ui-server.sh
+++ run-ui-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -DMODEL_URI=$MODEL_URI -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -classpath "./ui-server/build/libs/ui-server-1.0-SNAPSHOT.jar:./build/de.q60.mps.web.rcp/build/artifacts/de.q60.mps.webBranding/lib/*" de.q60.mps.web.ui.server.Main
+java -DMODEL_URI=$MODEL_URI -classpath "./ui-server/build/libs/ui-server-1.0-SNAPSHOT.jar:./build/de.q60.mps.web.rcp/build/artifacts/de.q60.mps.webBranding/lib/*" de.q60.mps.web.ui.server.Main
