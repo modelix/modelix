a03ca32f0762bb00919f1848d3c5e7ecf254d184
Sascha Lisson <sl@slisson.de>
2019-10-17 17:03:39 +0200

    Script for tagging and pushing docker images

--- docker-push-gcloud.sh
+++ docker-push-gcloud.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
-docker tag webmps-db eu.gcr.io/webmps/webmps-db
-docker tag webmps-model eu.gcr.io/webmps/webmps-model
-docker tag webmps-ui eu.gcr.io/webmps/webmps-ui
+TAG=$(date +"%Y%m%d%H%M")
 
-docker push eu.gcr.io/webmps/webmps-db
-docker push eu.gcr.io/webmps/webmps-model
-docker push eu.gcr.io/webmps/webmps-ui
+echo "Pushing tag ${TAG}"
+
+docker tag webmps-db "eu.gcr.io/webmps/webmps-db:${TAG}"
+docker tag webmps-model "eu.gcr.io/webmps/webmps-model:${TAG}"
+docker tag webmps-ui "eu.gcr.io/webmps/webmps-ui:${TAG}"
+
+docker push "eu.gcr.io/webmps/webmps-db:${TAG}"
+docker push "eu.gcr.io/webmps/webmps-model:${TAG}"
+docker push "eu.gcr.io/webmps/webmps-ui:${TAG}"
+
+docker tag webmps-db "eu.gcr.io/webmps/webmps-db:latest"
+docker tag webmps-model "eu.gcr.io/webmps/webmps-model:latest"
+docker tag webmps-ui "eu.gcr.io/webmps/webmps-ui:latest"
+
+docker push "eu.gcr.io/webmps/webmps-db:latest"
+docker push "eu.gcr.io/webmps/webmps-model:latest"
+docker push "eu.gcr.io/webmps/webmps-ui:latest"
+
+echo "Pushed tag ${TAG}"
+
+sed -i -E "s/20[12][0-9][01][0-9][012][0-9][0-5][0-9][0-5][0-9]/${TAG}/" kubernetes/common/ui-deployment.yaml
+sed -i -E "s/20[12][0-9][01][0-9][012][0-9][0-5][0-9][0-5][0-9]/${TAG}/" kubernetes/common/model-deployment.yaml
