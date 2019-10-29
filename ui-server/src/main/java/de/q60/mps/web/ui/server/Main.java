54b8c38ccc4062eb037b8ee614396e8854012939
Sascha Lisson <sl@slisson.de>
2019-10-29 10:35:01 +0100

    ui-server: log4j wasn't configured

--- ui-server/src/main/java/de/q60/mps/web/ui/server/Main.java
+++ ui-server/src/main/java/de/q60/mps/web/ui/server/Main.java
@@ -1,9 +1,18 @@
 package de.q60.mps.web.ui.server;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+    private static final Logger LOG = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOG.error("Test error");
+        LOG.warn("Test warning");
+        LOG.info("Test info");
+        LOG.debug("Test debug");
+        LOG.trace("Test trace");
         EnvironmentLoader.loadEnvironment();
-        System.out.println("idea.load.plugins.id: " + System.getProperty("idea.load.plugins.id"));
+        LOG.debug("idea.load.plugins.id: " + System.getProperty("idea.load.plugins.id"));
     }
 }
 
