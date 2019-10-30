167a456e1e197770a0b358b06e281cdea4bd65a8
Sascha Lisson <sl@slisson.de>
2019-10-30 11:06:06 +0100

    Add gcloud profiler to the ui server

--- run-ui-server.sh
+++ run-ui-server.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-java -DMODEL_URI=$MODEL_URI -classpath "./ui-server.jar:./mps/lib/*" de.q60.mps.web.ui.server.Main
+java -agentpath:/opt/cprof/profiler_java_agent.so=-cprof_service=ui \
+     -DMODEL_URI=$MODEL_URI \
+     -classpath "./ui-server.jar:./mps/lib/*" \
+     de.q60.mps.web.ui.server.Main
