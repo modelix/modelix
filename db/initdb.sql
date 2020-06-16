41c8b3146d9095203ae5ebc5241ea0f00c729c37
Sascha Lisson <sl@slisson.de>
2020-06-16 17:41:15 +0200

    Renaming in docker and kubernetes related files (2)

--- db/initdb.sql
+++ db/initdb.sql
@@ -1,11 +1,11 @@
-CREATE SCHEMA cloudmps;
-GRANT ALL ON SCHEMA cloudmps TO cloudmps;
+CREATE SCHEMA modelix;
+GRANT ALL ON SCHEMA modelix TO modelix;
 
-CREATE TABLE cloudmps.model
+CREATE TABLE modelix.model
 (
     key character varying NOT NULL,
     value character varying,
     reachable boolean,
     CONSTRAINT kv_pkey PRIMARY KEY (key)
 );
-GRANT ALL ON TABLE cloudmps.model TO cloudmps;
+GRANT ALL ON TABLE modelix.model TO modelix;
