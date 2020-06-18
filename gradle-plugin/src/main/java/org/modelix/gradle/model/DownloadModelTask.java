79f9b0ed4b166b2156149630c8949c3a5513c075
Sascha Lisson <sl@slisson.de>
2020-06-18 12:53:24 +0200

    Parameters for the downloadModel gradle task

--- gradle-plugin/src/main/java/org/modelix/gradle/model/DownloadModelTask.java
+++ gradle-plugin/src/main/java/org/modelix/gradle/model/DownloadModelTask.java
@@ -5,10 +5,29 @@ import org.gradle.api.tasks.TaskAction;
 
 public class DownloadModelTask extends DefaultTask {
 
+    private String serverUrl;
+    private String treeId;
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String getTreeId() {
+        return treeId;
+    }
+
+    public void setTreeId(String treeId) {
+        this.treeId = treeId;
+    }
+
     @TaskAction
     public void downloadModel() {
         System.out.println("modelix gradle plugin");
-        System.out.println("Downloading model ...");
+        System.out.println("Downloading tree " + getTreeId() + " from " + getServerUrl() + " ...");
         System.out.println("Done.");
     }
 
