17cb3ac7540a739c73bd9604f90367708843398b
Federico Tomassetti <federico@tomassetti.me>
2020-08-01 12:58:57 +0200

    complete conversions of api package

---
+++ model-client/src/main/kotlin/org/modelix/model/api/CompositeNodeResolveContext.kt
@@ -0,0 +1,31 @@
+package org.modelix.model.api
+
+import org.modelix.model.util.StreamUtils.toStream
+import java.util.*
+import java.util.function.Predicate
+import java.util.stream.Collectors
+
+class CompositeNodeResolveContext(contexts: Iterable<INodeResolveContext?>) : INodeResolveContext {
+    private val contexts: List<INodeResolveContext?>
+
+    constructor(vararg contexts: INodeResolveContext?) : this(contexts.toList()) {}
+
+    override fun resolve(ref: INodeReference?): INode? {
+        return contexts.stream().map { it?.resolve(ref) }.filter { obj: INode? -> Objects.nonNull(obj) }.findFirst().orElse(null)
+    }
+
+    companion object {
+        fun withAdditionalContext(context: INodeResolveContext?, runnable: Runnable?) {
+            val activeContext = INodeResolveContext.CONTEXT_VALUE.getValue()
+            if (activeContext == null) {
+                INodeResolveContext.CONTEXT_VALUE.runWith(context!!, runnable!!)
+            } else {
+                INodeResolveContext.CONTEXT_VALUE.runWith(CompositeNodeResolveContext(context, activeContext), runnable!!)
+            }
+        }
+    }
+
+    init {
+        this.contexts = contexts.toList()
+    }
+}
