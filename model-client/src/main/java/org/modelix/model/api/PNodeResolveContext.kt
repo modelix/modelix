e9eee5f59aeab1fd9dfd78d0e4db3e82dbb2becb
Federico Tomassetti <federico@tomassetti.me>
2020-08-01 12:41:44 +0200

    convert PNodeResolveContext

---
+++ model-client/src/main/java/org/modelix/model/api/PNodeResolveContext.kt
@@ -0,0 +1,13 @@
+package org.modelix.model.api
+
+class PNodeResolveContext(val branch: IBranch) : INodeResolveContext {
+
+    override fun resolve(ref: INodeReference?): INode? {
+        return if (ref is PNodeReference) {
+            PNodeAdapter(ref.id, branch)
+        } else {
+            null
+        }
+    }
+
+}
