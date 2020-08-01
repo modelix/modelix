1cdacfea99455c0b73d33b05ca0dbdb0a007ff30
Federico Tomassetti <federico@tomassetti.me>
2020-08-01 14:29:48 +0200

    translate CLElementRef

---
+++ model-client/src/main/java/org/modelix/model/lazy/CLElementRef.kt
@@ -0,0 +1,24 @@
+package org.modelix.model.lazy
+
+class CLElementRef(val id: Long) {
+
+    override fun equals(o: Any?): Boolean {
+        if (this === o) {
+            return true
+        }
+        if (o == null || this.javaClass != o.javaClass) {
+            return false
+        }
+        val that = o as CLElementRef
+        return if (id != that.id) {
+            false
+        } else true
+    }
+
+    override fun hashCode(): Int {
+        var result = 0
+        result = 31 * result + (id xor (id shr 32)).toInt()
+        return result
+    }
+
+}
