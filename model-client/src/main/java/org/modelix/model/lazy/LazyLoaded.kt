0ef1bd5f06c1ff290e74476f2fb00b55d33f2ab9
Federico Tomassetti <federico@tomassetti.me>
2020-08-01 15:54:47 +0200

    translate LazyLoaded

---
+++ model-client/src/main/java/org/modelix/model/lazy/LazyLoaded.kt
@@ -0,0 +1,22 @@
+package org.modelix.model.lazy
+
+import java.util.function.Function
+
+abstract class LazyLoaded<E>(protected var hash: String?, protected var store: IDeserializingKeyValueStore?) {
+    protected fun init() {
+        if (hash == null) {
+            return
+        }
+        try {
+            val deserialized = store!![hash, Function { serialized: String? -> deserialize(serialized) }]
+            init(deserialized)
+        } finally {
+            store = null
+            hash = null
+        }
+    }
+
+    protected abstract fun init(data: E?)
+    abstract fun deserialize(serialized: String?): E
+
+}
