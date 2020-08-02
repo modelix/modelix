7b17e1e6ee7dc8bc1cb1be9d6de5f3c76ec871aa
Federico Tomassetti <federico@tomassetti.me>
2020-08-02 09:04:54 +0200

    translate CLNode

---
+++ model-client/src/main/kotlin/org/modelix/model/lazy/CLNode.kt
@@ -0,0 +1,43 @@
+package org.modelix.model.lazy
+
+import org.modelix.model.persistent.CPElementRef
+import org.modelix.model.persistent.CPNode
+import java.util.function.Function
+import java.util.stream.Stream
+import java.util.stream.StreamSupport
+
+class CLNode(tree: CLTree?, data: CPNode?) : CLElement(tree!!, data!!) {
+    constructor(tree: CLTree?, id: Long, concept: String?, parentId: Long, roleInParent: String?, childrenIds: LongArray?, propertyRoles: Array<String?>?, propertyValues: Array<String?>?, referenceRoles: Array<String?>?, referenceTargets: Array<CPElementRef?>?) : this(tree, CPNode.create(id, concept, parentId, roleInParent, childrenIds, propertyRoles, propertyValues, referenceRoles, referenceTargets)) {}
+
+    override fun getData(): CPNode? {
+        return super.getData() as CPNode?
+    }
+
+    fun getChildren(bulkQuery: IBulkQuery?): IBulkQuery.Value<Iterable<CLNode>?>? {
+        return (getTree() as CLTree).resolveElements(getData()!!.childrenIds, bulkQuery).map(Function<List<CLNode>, Iterable<CLNode>?> { elements: List<CLNode>? -> elements })
+    }
+
+    fun getDescendants(bulkQuery: IBulkQuery, includeSelf: Boolean): IBulkQuery.Value<Iterable<CLNode>>? {
+        return if (includeSelf) {
+            getDescendants(bulkQuery, false)!!
+                    .map(Function { descendants: Iterable<CLNode> ->
+                        Iterable<CLNode> {
+                            Stream.concat(
+                                    Stream.of(this),
+                                    StreamSupport.stream(descendants.spliterator(), false)
+                            ).iterator()
+                        }
+                    })
+        } else {
+            getChildren(bulkQuery)!!.mapBulk(Function { children: Iterable<CLNode>? ->
+                val d = bulkQuery
+                        .map(children, Function { child: CLNode -> child.getDescendants(bulkQuery, true) })!!
+                        .map(Function { it: List<Iterable<CLNode>>? -> Iterable<CLNode> { it!!.stream().flatMap { n: Iterable<CLNode> -> StreamSupport.stream(n.spliterator(), false) }.iterator() } })
+                d
+            })
+        }
+    }
+
+    val concept: String?
+        get() = getData()!!.concept
+}
