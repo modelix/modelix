f28f6c839ff576018ba0f05e0c06e37545384576
Federico Tomassetti <federico@tomassetti.me>
2020-08-08 17:50:39 +0200

    support UUID on JS

--- model-client/src/jsMain/kotlin/org/modelix/model/PlatformSpecific.kt
+++ model-client/src/jsMain/kotlin/org/modelix/model/PlatformSpecific.kt
@@ -20,18 +20,22 @@ actual fun logDebug(message: () -> String, contextClass: KClass<*>) {
 
 actual fun bitCount(bits: Int): Int {
     var i = bits
-    i = i - (i ushr 1 and 0x55555555)
+    i -= (i ushr 1 and 0x55555555)
     i = (i and 0x33333333) + (i ushr 2 and 0x33333333)
     i = i + (i ushr 4) and 0x0f0f0f0f
-    i = i + (i ushr 8)
-    i = i + (i ushr 16)
+    i += (i ushr 8)
+    i += (i ushr 16)
     return i and 0x3f
 }
 
 actual fun <K, V> createLRUMap(size: Int): MutableMap<K, V> {
-    TODO("Not yet implemented")
+    return HashMap()
 }
 
+@JsModule("uuid")
+@JsNonModule
+external fun uuidv4() : String
+
 actual fun randomUUID(): String {
-    TODO("Not yet implemented")
+    return uuidv4();
 }
