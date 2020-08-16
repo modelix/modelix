3f5684d177d0dc9e55a228a8c0d6932c1ba9390e
Federico Tomassetti <federico@tomassetti.me>
2020-08-16 14:15:55 +0200

    make PNodeReference a data class

--- model-client/src/commonMain/kotlin/org/modelix/model/api/PNodeReference.kt
+++ model-client/src/commonMain/kotlin/org/modelix/model/api/PNodeReference.kt
@@ -15,7 +15,7 @@
 
 package org.modelix.model.api
 
-class PNodeReference(val id: Long) : INodeReference {
+data class PNodeReference(val id: Long) : INodeReference {
 
     override fun resolveNode(context: INodeResolveContext?): INode? {
         return if (context is PNodeResolveContext) {
@@ -25,24 +25,4 @@ class PNodeReference(val id: Long) : INodeReference {
         }
     }
 
-    override fun equals(o: Any?): Boolean {
-        if (this === o) {
-            return true
-        }
-        if (o == null || this::class != o::class) {
-            return false
-        }
-        val that = o as PNodeReference
-        return id == that.id
-    }
-
-    override fun hashCode(): Int {
-        var result = 0
-        result = 31 * result + (id xor (id shr 32)).toInt()
-        return result
-    }
-
-    override fun toString(): String {
-        return "PNodeReference{id=$id}"
-    }
 }
