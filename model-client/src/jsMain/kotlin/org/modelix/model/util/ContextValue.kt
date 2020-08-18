e2b8372d80fc8b76b985e1a260360f2b1d061b6f
Sascha Lisson <sl@slisson.de>
2020-08-18 09:18:21 +0200

    Implemented ContextValue for JS

--- model-client/src/jsMain/kotlin/org/modelix/model/util/ContextValue.kt
+++ model-client/src/jsMain/kotlin/org/modelix/model/util/ContextValue.kt
@@ -1,22 +1,31 @@
 package org.modelix.model.util
 
 actual class ContextValue<E> {
+    private var value: E?
+
     actual constructor() {
-        TODO("Not yet implemented")
+        value = null
     }
 
     actual constructor(defaultValue: E) {
-        TODO("Not yet implemented")
+        value = defaultValue
     }
 
     actual fun getValue(): E? {
-        TODO("Not yet implemented")
+        return value
     }
 
     actual fun runWith(newValue: E, r: () -> Unit) {
+        computeWith(newValue, r)
     }
 
     actual fun <T> computeWith(newValue: E, r: () -> T): T {
-        TODO("Not yet implemented")
+        val oldValue = value
+        value = newValue
+        try {
+            return r()
+        } finally {
+            value = oldValue
+        }
     }
 }
