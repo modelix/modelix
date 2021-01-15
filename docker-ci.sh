770a32e2300f2c63e4a634ba70d30b73300e7bb8
Sascha Lisson <sl@slisson.de>
2021-01-15 11:43:32 +0100

    Publish docker image tags containing the mps version
    There will be three docker image tags for each release. One with the git
    tag (0.0.12), one for the MPS major version (2020.3) and one for the
    full MPS version (2020.3.4)
    
    (cherry picked from commit 9691c472c21ba521b9dea3a5be55a0a3306e83fc)

--- docker-ci.sh
+++ docker-ci.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+# read variables from mps-version.properties
+while IFS='=' read -r key value
+do
+  echo "$key=$value"
+  eval "${key}"=\${value}
+done < "mps-version.properties"
+echo "MPS Version: $mpsVersion"
+echo "MPS Major Version: $mpsMajorVersion"
+echo "MPS Minor Version: $mpsMinorVersion"
 
 docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_KEY"
 
@@ -9,23 +19,28 @@ docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_KEY"
 ./docker-build-proxy.sh
 ./docker-build-uiproxy.sh
 
-TAG="$TRAVIS_TAG"
+GIT_TAG="$TRAVIS_TAG"
+TAGS="$GIT_TAG $mpsVersion"
+if [ "$mpsVersion" != "$mpsMajorVersion" ]; then
+    TAGS="$TAGS $mpsMajorVersion"
+fi
+for TAG in $TAGS ; do
+  echo "Pushing Tag $TAG"
 
-echo "Pushing tag ${TAG}"
+  docker tag modelix/modelix-db:latest "modelix/modelix-db:${TAG}"
+  docker tag modelix/modelix-model:latest "modelix/modelix-model:${TAG}"
+  docker tag modelix/modelix-mps:latest "modelix/modelix-mps:${TAG}"
+  docker tag modelix/modelix-ui:latest "modelix/modelix-ui:${TAG}"
+  docker tag modelix/modelix-proxy:latest "modelix/modelix-proxy:${TAG}"
+  docker tag modelix/modelix-uiproxy:latest "modelix/modelix-uiproxy:${TAG}"
 
-docker tag modelix/modelix-db:latest "modelix/modelix-db:${TAG}"
-docker tag modelix/modelix-model:latest "modelix/modelix-model:${TAG}"
-docker tag modelix/modelix-mps:latest "modelix/modelix-mps:${TAG}"
-docker tag modelix/modelix-ui:latest "modelix/modelix-ui:${TAG}"
-docker tag modelix/modelix-proxy:latest "modelix/modelix-proxy:${TAG}"
-docker tag modelix/modelix-uiproxy:latest "modelix/modelix-uiproxy:${TAG}"
-
-docker push "modelix/modelix-db:${TAG}"
-docker push "modelix/modelix-model:${TAG}"
-docker push "modelix/modelix-mps:${TAG}"
-docker push "modelix/modelix-ui:${TAG}"
-docker push "modelix/modelix-proxy:${TAG}"
-docker push "modelix/modelix-uiproxy:${TAG}"
+  docker push "modelix/modelix-db:${TAG}"
+  docker push "modelix/modelix-model:${TAG}"
+  docker push "modelix/modelix-mps:${TAG}"
+  docker push "modelix/modelix-ui:${TAG}"
+  docker push "modelix/modelix-proxy:${TAG}"
+  docker push "modelix/modelix-uiproxy:${TAG}"
+done
 
 docker push "modelix/modelix-db:latest"
 docker push "modelix/modelix-model:latest"
