b61b124c8b68c58989277e4274390c7f02984eb9
Sascha Lisson <sl@slisson.de>
2021-10-28 14:58:11 +0200

    docker image and kubernetes configs for projector

--- kubernetes-use-latest-tag.sh
+++ kubernetes-use-latest-tag.sh
@@ -7,10 +7,12 @@ sed -i.bak -E "s/(image:.*:).*/\1${TAG}/" kubernetes/local/db-deployment.yaml
 sed -i.bak -E "s/(image:.*:).*/\1${TAG}/" kubernetes/common/model-deployment.yaml
 sed -i.bak -E "s/(image:.*:).*/\1${TAG}/" kubernetes/common/proxy-deployment.yaml
 sed -i.bak -E "s/(image:.*:).*/\1${TAG}/" kubernetes/common/ui-deployment.yaml
+sed -i.bak -E "s/(image:.*:).*/\1${TAG}/" kubernetes/common/projector-deployment.yaml
 sed -i.bak -E "s/(image:.*:).*/\1${TAG}/" kubernetes/common/uiproxy-deployment.yaml
 
 rm kubernetes/local/db-deployment.yaml.bak
 rm kubernetes/common/model-deployment.yaml.bak
 rm kubernetes/common/proxy-deployment.yaml.bak
 rm kubernetes/common/ui-deployment.yaml.bak
+rm kubernetes/common/projector-deployment.yaml.bak
 rm kubernetes/common/uiproxy-deployment.yaml.bak
