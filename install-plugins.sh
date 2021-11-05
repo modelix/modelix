47ed63108547151d46dc23ea7b7fa781d3c478fb
Sascha Lisson <sl@slisson.de>
2021-11-05 23:10:40 +0100

    Script that takes care of extracting and copying MPS plugins
    In your Dockerfile you can just copy everything to /mps-plugins and call
    /install-plugins.sh. After extracting all ZIP files, the script searches
    for META-INF/plugin.xml and then copies the correct folder to the
    correct location. This fixes the issue that plugins are not loaded,
    because some COPY statement in the Dockerfile was wrong.

---
+++ install-plugins.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Finds and copies all MPS plugins from SOURCE_FOLDER to TARGET_FOLDER. Zip files containing plugins are extracted.
+
+TARGET_FOLDER="${1:-/usr/modelix-ui/mps/plugins/}"
+SOURCE_FOLDER="${2:-/mps-plugins/}"
+
+#SOURCE_FOLDER=../test_input
+#TARGET_FOLDER=../test_output/
+#rm -rf $SOURCE_FOLDER
+#rm -rf $TARGET_FOLDER
+#mkdir $SOURCE_FOLDER
+#mkdir $TARGET_FOLDER
+#cp -R ./build $SOURCE_FOLDER
+
+find $SOURCE_FOLDER -name '*.zip' |
+while IFS= read -r line; do
+  ZIP_OUTPUT_DIR="$line-unzip"
+  unzip -d $ZIP_OUTPUT_DIR $line
+done
+
+find $SOURCE_FOLDER -path '**/META-INF/plugin.xml' |
+while IFS= read -r line; do
+  PLUGIN_DIR="$(dirname "$(dirname $line)")"
+  mv $PLUGIN_DIR $TARGET_FOLDER
+  echo "Installed plugin from $PLUGIN_DIR"
+done
