30f745f3250b5dc74812d81d6c2650346a7df077
Kolja Dummann <kolja@hey.com>
2021-11-23 09:44:08 +0100

    ui-client: update NPM dependencies
    Updates npm dependencies in ui-client to be able to build with newer
    node versions. Node 12 isn't available for aarch64 (Apple M1) and building
    with a newer local node version would fail because of libscss being
    outdated and not building. While updating webpack the the extract-text
    plugin was replaces with mini-css-extract because extract-text is
    deprecated.

--- ui-client/src/scripts/DeprecatedNewNodeFromModel.ts
+++ ui-client/src/scripts/DeprecatedNewNodeFromModel.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import $ = require("jquery");
 
 export class DeprecatedNewNodeFromModel {
--- ui-client/webpack/plugins.js
+++ ui-client/webpack/plugins.js
@@ -2,28 +2,22 @@
 
 var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 var ENV = process.env.npm_lifecycle_event;
 var isProd = ENV === 'build';
 
 module.exports = [
-  new webpack.ProgressPlugin(),
+    new webpack.ProgressPlugin(),
 
-  new HtmlWebpackPlugin({
-    template: 'public/index.html',
-    chunksSortMode: 'auto'
-  }),
+    new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        chunksSortMode: 'auto'
+    }),
 
-  new ExtractTextPlugin({
-    filename: 'css/[name].css',
-    disable: !isProd
-  }),
-
-  new CopyWebpackPlugin({
-    patterns: [
-        {from: 'public'}
-    ]
-  })
+    new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+    })
 ];
