3356d9eb9a4305e46f77a3476c84097ab58988af
Sascha Lisson <sl@slisson.de>
2021-11-30 19:01:37 +0100

    Dockerfile for project split into two
    To avoid running "apt install unzip" each time there is a change in modelix.

--- docker-build-projector.sh
+++ docker-build-projector.sh
@@ -4,17 +4,6 @@ set -e
 
 TAG=$( ./modelix-version.sh )
 
-# patch branding.jar to not show EULA and data sharing agreement at startup
-rm -rf build/branding
-rm -f build/branding.zip
-unzip -d build/branding artifacts/mps/lib/branding.jar
-(
-  cd build/branding
-  sed -i.bak -E "s/JetBrains s.r.o./Modelix/" idea/IdeaApplicationInfo.xml
-  rm idea/IdeaApplicationInfo.xml.bak
-  zip -r ../branding.zip ./*
-)
-
 docker build --no-cache -f Dockerfile-projector -t modelix/modelix-projector .
 
 docker tag modelix/modelix-projector:latest "modelix/modelix-projector:${TAG}"
