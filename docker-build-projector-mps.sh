a585ab636b6822f7d0b2cdb9c5706400f6a465d8
Sascha Lisson <sl@slisson.de>
2022-05-05 17:39:13 +0200

    projector was still trying to download pako.js from the CDN
    The docker build script wasn't using the local patched projector-client,
    but downloading the official version instead.

--- docker-build-projector-mps.sh
+++ docker-build-projector-mps.sh
@@ -29,22 +29,31 @@ TIMESTAMP="$(date +"%Y%m%d%H%M")"
   else
     git clone https://github.com/JetBrains/projector-server.git
   fi
+  (
+    cd projector-server
+    echo "useLocalProjectorClient=true" > local.properties
+  )
+
   if [ -d ./projector-client ]; then
     (
       cd projector-client
       git reset --hard
       git clean -xdf
       git pull
-
-      # If the internet connection is restricted trying to downloading pako.min.js causes the browser to do nothing
-      # until the connection times out. This appears like the link for opening a node in projector is broken.
-      cd projector-client-web/src/main/resources
-      wget https://cdn.jsdelivr.net/pako/1.0.3/pako.min.js
-      sed -i.bak -E "s/https:\/\/cdn.jsdelivr.net\/pako\/1.0.3\/pako.min.js/pako.min.js/" index.html
     )
   else
     git clone https://github.com/JetBrains/projector-client.git
   fi
+  (
+    cd projector-client
+
+    # If the internet connection is restricted trying to downloading pako.min.js causes the browser to do nothing
+    # until the connection times out. This appears like the link for opening a node in projector is broken.
+    cd projector-client-web/src/main/resources
+    wget https://cdn.jsdelivr.net/pako/1.0.3/pako.min.js
+    sed -i.bak -E "s/https:\/\/cdn.jsdelivr.net\/pako\/1.0.3\/pako.min.js/pako.min.js/" index.html
+  )
+
   if [ -d ./projector-docker ]; then
     (
       cd projector-docker
@@ -57,7 +66,7 @@ TIMESTAMP="$(date +"%Y%m%d%H%M")"
   fi
 
   cd projector-docker
-  ./build-container.sh "projector-mps" "https://download.jetbrains.com/mps/${mpsMajorVersion}/MPS-${mpsVersion}.tar.gz"
+  ./build-container-dev.sh "projector-mps" "https://download.jetbrains.com/mps/${mpsMajorVersion}/MPS-${mpsVersion}.tar.gz"
 )
 
 
