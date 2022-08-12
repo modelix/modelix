0bb6ecce1cc335f34213d86ce132c79354a0eeb9
Sascha Lisson <sl@slisson.de>
2022-08-12 22:02:59 +0200

    fixed model synchronizer (33)

--- model-client/src/commonMain/kotlin/org/modelix/model/lazy/NodeNotFoundException.kt
+++ model-client/src/commonMain/kotlin/org/modelix/model/lazy/NodeNotFoundException.kt
@@ -2,6 +2,5 @@ package org.modelix.model.lazy
 
 import org.modelix.model.persistent.SerializationUtil
 
-class NodeNotFoundException(nodeId: Long)
-    : RuntimeException("Element doesn't exist: ${SerializationUtil.longToHex(nodeId)} ($nodeId)") {
-}
+class NodeNotFoundException(nodeId: Long) :
+    RuntimeException("Element doesn't exist: ${SerializationUtil.longToHex(nodeId)} ($nodeId)")
