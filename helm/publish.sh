49ccf2377086f92367b63eee3407bdd39ea757a2
Sascha Lisson <sl@slisson.de>
2022-08-18 18:55:46 +0200

    Dependency versions in the helm chart were accidentally replaced with the Modelix version

--- helm/publish.sh
+++ helm/publish.sh
@@ -8,8 +8,8 @@ cd "$(dirname "$0")"
 TAG=$( ../modelix-version.sh )
 
 
-sed -i.bak -E "s/appVersion:.*/appVersion: \"${TAG}\"/" modelix/Chart.yaml
-sed -i.bak -E "s/version:.*/version: \"${TAG}\"/" modelix/Chart.yaml
+sed -i.bak -E "s/^appVersion:.*/appVersion: \"${TAG}\"/" modelix/Chart.yaml
+sed -i.bak -E "s/^version:.*/version: \"${TAG}\"/" modelix/Chart.yaml
 rm modelix/Chart.yaml.bak
 
 mkdir -p repo
