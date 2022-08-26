f7b687368ee62c8f6afe0c52372ef2926158b984
Sascha Lisson <sl@slisson.de>
2022-08-26 18:56:54 +0200

    moved meta model generator to the model-api repo
    The generator project has to be built separately
    before it can be used. The new location seems
    to be a good fit.

--- metamodel-sandbox/build.gradle.kts
+++ metamodel-sandbox/build.gradle.kts
@@ -1,12 +1,43 @@
+import org.modelix.metamodel.definition.Language
+import org.modelix.metamodel.generator.KotlinGenerator
+
+buildscript {
+    repositories {
+        mavenLocal()
+        maven { url = uri("https://artifacts.itemis.cloud/repository/maven-mps/") }
+    }
+    dependencies {
+        val modelApiVersion: String by rootProject
+        classpath("org.modelix:metamodel-generator:$modelApiVersion")
+    }
+}
+
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
 }
 
 dependencies {
-    implementation(project(":metamodel-runtime"))
+    val modelApiVersion: String by rootProject
+    implementation("org.modelix:metamodel-runtime:$modelApiVersion")
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
 }
 
-sourceSets["main"].java.srcDir(file("src/main/kotlin_gen"))
+val generatorOutputDir = file("src/main/kotlin_gen")
+sourceSets["main"].java.srcDir(generatorOutputDir)
+
+val generateMetaModelSources = tasks.create("generateMetaModelSources") {
+    val languageFile = file("src/main/resources/EntitiesLanguage.yaml")
+    inputs.file(languageFile)
+    outputs.dir(generatorOutputDir)
+    doLast {
+        KotlinGenerator(generatorOutputDir.toPath()).generate(listOf(
+            Language.fromFile(languageFile)
+        ))
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn(generateMetaModelSources)
+}
