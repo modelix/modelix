c8dc6b686f3192e418fe7755b72c9f02aae3e2d5
Michael Huster <mhuster@itemis.com>
2023-02-10 16:44:17 +0100

    cleanup

--- docker-build-base.sh
+++ docker-build-base.sh
@@ -3,5 +3,5 @@
 TAG=$( ./modelix-version.sh )
 MODELIX_TARGET_PLATFORM="${MODELIX_TARGET_PLATFORM:=linux/amd64}"
 
-docker build --platform ${MODELIX_TARGET_PLATFORM:-linux/amd64} --no-cache -f Dockerfile-base -t modelix/modelix-base .
+docker build --platform ${MODELIX_TARGET_PLATFORM} --no-cache -f Dockerfile-base -t modelix/modelix-base .
 docker tag modelix/modelix-base:latest "modelix/modelix-base:${TAG}"
