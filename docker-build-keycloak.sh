fc0483925f01bf0f38854968165ccbee6104259f
Michael Huster <mhuster@itemis.com>
2023-02-15 11:19:43 +0100

    fixed keycloak build

--- docker-build-keycloak.sh
+++ docker-build-keycloak.sh
@@ -7,7 +7,7 @@ TAG=$( ./modelix-version.sh )
 (
   cd keycloak-extensions
   docker buildx build --platform linux/amd64,linux/arm64 --push \
-  -t modelix/keycloak:latest -t modelix/keycloak:${TAG}.
+  -t modelix/keycloak:latest -t modelix/keycloak:${TAG} .
 )
 
 sed -i.bak -E "s/  keycloak: \".*\"/  keycloak: \"${TAG}\"/" helm/dev.yaml
