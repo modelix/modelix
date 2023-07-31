e93ec996bad40d6675e45abe191811e927f3ebab
Sascha Lisson <sascha.lisson@itemis.de>
2023-07-31 16:52:49 +0200

    patch branding.jar inside docker as a separate build stage

--- docker-build-projector-base.sh
+++ docker-build-projector-base.sh
@@ -14,17 +14,6 @@ echo "MPS Version: $mpsVersion"
 echo "MPS Major Version: $mpsMajorVersion"
 echo "MPS Minor Version: $mpsMinorVersion"
 
-# patch branding.jar to not show EULA and data sharing agreement at startup
-rm -rf build/branding
-rm -f build/branding.zip
-unzip -d build/branding artifacts/mps/lib/branding.jar
-(
-  cd build/branding
-  sed -i.bak -E "s/JetBrains s.r.o./Modelix/" idea/IdeaApplicationInfo.xml
-  rm idea/IdeaApplicationInfo.xml.bak
-  zip -r ../branding.zip ./*
-)
-
 if [ "${CI}" = "true" ]; then
   docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg MPS_VERSION=${mpsVersion} -f Dockerfile-projector-base \
   -t "modelix/modelix-projector-base:${MPS_VERSION}" .
