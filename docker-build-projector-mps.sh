df9a4af342e810eb0b93373a494a1bfa3f5d4540
Sascha Lisson <sascha.lisson@itemis.de>
2023-07-31 16:04:30 +0200

    downloading pako.min.js is no done in the projector-client repo

--- docker-build-projector-mps.sh
+++ docker-build-projector-mps.sh
@@ -44,15 +44,6 @@ TIMESTAMP="$(date +"%Y%m%d%H%M")"
   else
     git clone https://github.com/modelix/projector-client.git
   fi
-  (
-    cd projector-client
-
-    # If the internet connection is restricted trying to downloading pako.min.js causes the browser to do nothing
-    # until the connection times out. This appears like the link for opening a node in projector is broken.
-    cd projector-client-web/src/main/resources
-    wget https://cdn.jsdelivr.net/pako/1.0.3/pako.min.js
-    sed -i.bak -E "s/https:\/\/cdn.jsdelivr.net\/pako\/1.0.3\/pako.min.js/pako.min.js/" index.html
-  )
 
   if [ -d ./projector-docker ]; then
     (
